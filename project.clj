a177da1e3e5c328f85c8d2d627c20eeb4106560c
Ben Griffiths <benjamin.griffiths@nokia.com>
2012-10-02 15:23:03 +0100

    Updated project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject rest-cljer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject rest-cljer "0.1.0"
+  :description "A Clojure wrapper for the rest driver library"
+  :url "https://github.com/whostolebenfrog/rest-cljer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
