90c80d9891d555311b17d2a403f211cbfdd6ff09
Ben Griffiths <mrbengriffiths@googlemail.com>
2013-01-21 21:28:15 +0000

    Prepare project for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rest-cljer "0.1.3"
+(defproject rest-cljer "0.1.4-SNAPSHOT"
   :description "A Clojure wrapper for the rest driver library"
   :url "https://github.com/whostolebenfrog/rest-cljer"
   :license {:name "Eclipse Public License"
