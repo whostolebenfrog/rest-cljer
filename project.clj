546f856be891365ee16d81e86a2a278cf70e66d2
Ben Griffiths <benjamin.griffiths@nokia.com>
2013-08-20 10:39:26 +0100

    Update version for next release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rest-cljer "0.1.9"
+(defproject rest-cljer "0.1.10-SNAPSHOT"
   :description "A Clojure wrapper for the rest driver library"
   :url "https://github.com/whostolebenfrog/rest-cljer"
   :license {:name "Eclipse Public License"
