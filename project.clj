4255276956e35ab19a31b607c44c4b22aba1339b
Ben Griffiths <benjamin.griffiths@nokia.com>
2014-08-18 14:04:49 +0100

    lein-release plugin: bumped version from 0.1.15 to 0.1.16-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rest-cljer "0.1.15"
+(defproject rest-cljer "0.1.16-SNAPSHOT"
   :description "A Clojure wrapper for the rest driver library"
   :url "https://github.com/whostolebenfrog/rest-cljer"
   :license {:name "Eclipse Public License"
