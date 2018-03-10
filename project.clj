2e50f33746fe5014a716be54a7ed9ae65bbac4d0
Joe Littlejohn <joelittlejohn@gmail.com>
2018-03-10 20:10:28 +0000

    Update outdated dependencies

--- project.clj
+++ project.clj
@@ -4,18 +4,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[cheshire "5.7.0"]
-                 [clj-http "2.3.0"]
+  :dependencies [[cheshire "5.8.0"]
+                 [clj-http "3.8.0"]
                  [com.github.rest-driver/rest-client-driver "1.1.42" :exclusions [org.slf4j/slf4j-nop]]
                  [environ "1.1.0"]
-                 [junit "4.11"]
+                 [junit "4.12"]
                  [org.clojure/clojure "1.8.0"]]
 
   :lein-release {:deploy-via :clojars}
 
   :profiles {:dev {:plugins [[lein-midje "3.1.0"]
-                             [pjstadig/humane-test-output "0.8.1"]
+                             [pjstadig/humane-test-output "0.8.2"]
                              [lein-release "1.0.5"]
                              [lein-rpm "0.0.4"]]
-                   :dependencies [[midje "1.5.1"]
+                   :dependencies [[midje "1.9.1"]
                                   [speclj "3.3.2"]]}})
